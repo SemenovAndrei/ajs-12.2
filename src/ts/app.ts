553b954ad6d240720f2010e6b976fb8fdbebb8dd
Andrey Semenov <andss82@gmail.com>
2020-10-31 01:30:26 +0300

    add tests

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -11,3 +11,12 @@ cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
 cart.add(new Movie(123, 'Avengers', 5000, '2012', 'USA', 'Avengers Assemble!', ['action', 'fantasy', 'adventure'], '137 min. \\ 02:17'));
 
 console.log(cart.items);
+
+console.log(cart.getSum());
+console.log(cart.getSumWithDiscount());
+console.log(cart.getSumWithDiscount(10));
+
+cart.deleteItem(1001);
+console.log(cart.items);
+
+
